540d09688a15a7977fe0d535aea9a158cd545eac
widejoy <rogerantony40@ug.cusat.ac.in>
2023-04-16 12:16:46 +0530

    now faces can be outlined

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import face_recognition
 from PIL import Image,ImageDraw
 image_1 = face_recognition.load_image_file('./faces/faces.webp')
 image_1_loc = face_recognition.face_locations(image_1)
+image_1_pil = Image.fromarray(image_1)
 for i in range(0,len(image_1_loc)):
 
  top = image_1_loc[i][0]
@@ -9,6 +10,6 @@ for i in range(0,len(image_1_loc)):
  bottom = image_1_loc[i][2]
  left = image_1_loc[i][3]
 
- image_1_biden = image_1[top:bottom , left:right]
- image_1_joe = Image.fromarray(image_1_biden) 
- image_1_joe.show()
+ draw = ImageDraw.Draw(image_1_pil)
+ draw.rectangle((left,top,right,bottom),outline=(250,7,0))
+image_1_pil.show()
